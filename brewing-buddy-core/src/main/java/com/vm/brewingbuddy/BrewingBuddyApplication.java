bcba8db8aadc7fcf60fbd131b8bcfe9dddc2208d
DHodaniewicz <d.hodaniewicz@gmail.com>
2022-12-01 17:27:36 +0100

    feature: Added brewing-buddy-core files

---
+++ brewing-buddy-core/src/main/java/com/vm/brewingbuddy/BrewingBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.vm.brewingbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewingBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewingBuddyApplication.class, args);
+    }
+
+}
---
+++ brewing-buddy-core/src/test/java/com/vm/brewingbuddy/BrewingBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.brewingbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewingBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
