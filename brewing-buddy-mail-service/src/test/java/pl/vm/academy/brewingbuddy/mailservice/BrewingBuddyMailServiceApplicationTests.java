01fba5add46127d4ccf76ddb1cef89172651af16
Wojciech_Dudziak <wojciech.dudziak@vm.pl>
2023-01-04 17:41:47 +0100

    feature: created new spring mail-service module

---
+++ brewing-buddy-mail-service/src/test/java/pl/vm/academy/brewingbuddy/mailservice/BrewingBuddyMailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vm.academy.brewingbuddy.mailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewingBuddyMailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
