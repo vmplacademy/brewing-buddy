8a51ac831dcc298ecf723be652114891cef07842
DHodaniewicz <d.hodaniewicz@gmail.com>
2023-05-07 11:24:09 +0200

    fix: changed CalculatedParametersDto into a record

--- brewing-buddy-core/src/main/java/pl/vm/academy/brewingbuddy/core/business/recipe/dto/CalculatedParametersDto.java
+++ brewing-buddy-core/src/main/java/pl/vm/academy/brewingbuddy/core/business/recipe/dto/CalculatedParametersDto.java
@@ -1,29 +1,25 @@
 package pl.vm.academy.brewingbuddy.core.business.recipe.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class CalculatedParametersDto {
-    private BigDecimal mashingFactorInLitersPerKg;
-    private BigDecimal waterRequiredForMashingInLiters;
-    private BigDecimal waterRequiredForSpargingInLiters;
-    private BigDecimal waterRequiredForWholeProcessInLiters;
-    private BigDecimal amountOfWaterBeforeBoilingInLiters;
-    private BigDecimal extractBeforeBoilingInPercentage;
-    private BigDecimal amountOfHotWort;
 
-    private BigDecimal calculatedIbu;
-    private BigDecimal calculatedColourEBC;
-    private BigDecimal calculatedExtractInPercentage;
-    private BigDecimal estimatedAmountOfAlcoholAfterFermentation;
-}
+@Builder
+public record CalculatedParametersDto(
+        BigDecimal mashingFactorInLitersPerKg,
+        BigDecimal waterRequiredForMashingInLiters,
+        BigDecimal waterRequiredForSpargingInLiters,
+        BigDecimal waterRequiredForWholeProcessInLiters,
+        BigDecimal amountOfWaterBeforeBoilingInLiters,
+        BigDecimal extractBeforeBoilingInPercentage,
+        BigDecimal amountOfHotWort,
+
+        BigDecimal calculatedIbu,
+        BigDecimal calculatedColourEBC,
+        BigDecimal calculatedExtractInPercentage,
+        BigDecimal estimatedAmountOfAlcoholAfterFermentation
+) {}
+
+
