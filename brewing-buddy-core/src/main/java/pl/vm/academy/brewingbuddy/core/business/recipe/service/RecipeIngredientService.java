ac281d0727ccd9017dbf49c32d87c89467c8485e
DHodaniewicz <d.hodaniewicz@gmail.com>
2023-10-08 12:40:27 +0200

    feature: Added docker file

--- brewing-buddy-core/src/main/java/pl/vm/academy/brewingbuddy/core/business/recipe/service/RecipeIngredientService.java
+++ brewing-buddy-core/src/main/java/pl/vm/academy/brewingbuddy/core/business/recipe/service/RecipeIngredientService.java
@@ -11,11 +11,52 @@ import java.util.UUID;
 
 public interface RecipeIngredientService {
 
+    /**
+     *
+     * @param recipeHopDto
+     * @return RecipeDetailedDto
+     */
     RecipeDetailedDto addHopToRecipe (RecipeHopDto recipeHopDto);
+
+    /**
+     *
+     * @param recipeId
+     * @return Set<RecipeHopDto>
+     */
     Set<RecipeHopDto> getAllRecipeHopFromRecipe (UUID recipeId);
+
+    /**
+     *
+     * @param recipeMaltDto
+     * @return RecipeDetailedDto
+     */
     RecipeDetailedDto addMaltToRecipe (RecipeMaltDto recipeMaltDto);
+
+    /**
+     *
+     * @param recipeId
+     * @return Set <RecipeMaltDto>
+     */
     Set <RecipeMaltDto> getAllRecipeMaltsFromRecipe (UUID recipeId);
+
+    /**
+     *
+     * @param recipeExtraIngredientDto
+     * @return RecipeDetailedDto
+     */
     RecipeDetailedDto addExtraIngredientToRecipe (RecipeExtraIngredientDto recipeExtraIngredientDto);
+
+    /**
+     *
+     * @param recipeId
+     * @return Set <RecipeExtraIngredientDto>
+     */
     Set <RecipeExtraIngredientDto> getAllRecipeExtraIngredientsFromRecipe (UUID recipeId);
+
+    /**
+     *
+     * @param recipeYeastDto
+     * @return RecipeDetailedDto
+     */
     RecipeDetailedDto addYeastToRecipe (RecipeYeastDto recipeYeastDto);
 }
