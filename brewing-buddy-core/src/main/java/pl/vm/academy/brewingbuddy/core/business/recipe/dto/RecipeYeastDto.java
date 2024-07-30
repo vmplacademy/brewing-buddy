0a6dd3aa852065c8ddffed2b085a88d057667d0b
DHodaniewicz <d.hodaniewicz@gmail.com>
2024-07-30 10:22:58 +0200

    feature: Implemented Builder pattern for RecipeYeastDto

--- brewing-buddy-core/src/main/java/pl/vm/academy/brewingbuddy/core/business/recipe/dto/RecipeYeastDto.java
+++ brewing-buddy-core/src/main/java/pl/vm/academy/brewingbuddy/core/business/recipe/dto/RecipeYeastDto.java
@@ -2,12 +2,11 @@ package pl.vm.academy.brewingbuddy.core.business.recipe.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+import lombok.NoArgsConstructor;
 
-@Builder
 public record RecipeYeastDto(
         UUID id,
         @NotNull
@@ -17,4 +16,43 @@ public record RecipeYeastDto(
         @NotBlank
         UUID yeastId,
         BigDecimal yeastQuantity
-) {}
+) {
+        @NoArgsConstructor
+        public static final class Builder {
+                UUID id;
+                UUID recipeId;
+                UUID yeastId;
+                BigDecimal yeastQuantity;
+
+                public Builder id(UUID id) {
+                        this.id = id;
+                        return this;
+                }
+
+                public Builder recipeId(UUID recipeId) {
+                        this.recipeId = recipeId;
+                        return this;
+                }
+
+                public Builder yeastId(UUID yeastId) {
+                        this.yeastId = yeastId;
+                        return this;
+                }
+
+                public Builder yeastQuantity(BigDecimal yeastQuantity) {
+                        this.yeastQuantity = yeastQuantity;
+                        return this;
+                }
+
+
+                public RecipeYeastDto build() {
+                        return new RecipeYeastDto(id, recipeId, yeastId, yeastQuantity);
+                }
+
+        }
+
+        public static Builder builder() {
+             return new Builder();
+        }
+
+}
